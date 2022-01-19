9978439bdd51f2a9a505d51bcbb5c11ceeeedb1b
руслан <82108672+Ruslan-Tuliabaeiev@users.noreply.github.com>
2022-01-19 20:14:13 +0200

    2

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,27 +1,45 @@
-import { useState, useEffect  } from 'react';
+// import { useState, useEffect  } from 'react';
 
 
 
+
+// export const useLocalStorage = (key, defValue) => {
+
+//     const [state, setstate] = useState(() => {
+//         return (
+//             JSON.parse(localStorage.getItem('contacts')) ?? [
+//             { "id": "id-1", "name": "Rosie Simpson", "number": "459-12-56" },
+//             { "id": "id-2", "name": "Hermione Kline", "number": "443-89-12" },
+//             { "id": "id-3", "name": "Eden Clements", "number": "645-17-79" },
+//             { "id": "id-4", "name": "Annie Copeland", "number": "227-91-26" },
+//         ]);
+    
+//     })
+//     useEffect(() => {
+//         localStorage.setItem('contacts', JSON.stringify(state))
+//     }, [state]);
+//     return [state, setstate]
+
+
+
+// }
+
+
+import { useEffect, useState } from 'react';
 
 export const useLocalStorage = (key, defValue) => {
-
-    const [state, setstate] = useState(() => {
+    const [state, setState] = useState(() => {
         return (
             JSON.parse(localStorage.getItem('contacts')) ?? [
-            { "id": "id-1", "name": "Rosie Simpson", "number": "459-12-56" },
-            { "id": "id-2", "name": "Hermione Kline", "number": "443-89-12" },
-            { "id": "id-3", "name": "Eden Clements", "number": "645-17-79" },
-            { "id": "id-4", "name": "Annie Copeland", "number": "227-91-26" },
-        ]);
-    
-    })
+                { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+                { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+                { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+                { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+            ]
+        );
+    });
     useEffect(() => {
-        localStorage.setItem('contacts', JSON.stringify(state))
+        localStorage.setItem('contacts', JSON.stringify(state));
     }, [state]);
-    return [state, setstate]
-
-
-
-}
-
-// export default useLocalStorage;
+    return [state, setState];
+};
